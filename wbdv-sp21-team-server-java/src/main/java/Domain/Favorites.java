bff4263fa2aaab390e5b6c1bfab37de467520c65
Jinghe Zhang <jinghezhang@Jinghes-MacBook-Pro.local>
2021-04-02 22:54:46 -0500

    Add lombok plugin for constructors and getters and etc.

--- wbdv-sp21-team-server-java/src/main/java/Domain/Favorites.java
+++ wbdv-sp21-team-server-java/src/main/java/Domain/Favorites.java
@@ -1,4 +1,16 @@
 package Domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Favorites {
+    private String user;
+    private String nameOfArtWork;
+    private String idOfArtwork;
+    private String urlOfArtwork;
 }
